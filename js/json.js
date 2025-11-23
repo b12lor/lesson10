8af0736be088cd4302550820122b296da30f1dff
Ilori Jonathan <ilorijonathan947@gmail.com>
2025-11-23 01:27:33 -0500

    Updated files

--- js/json.js
+++ js/json.js
@@ -1,63 +1,57 @@
-/* STEP 2: Reference the HEADER and the SECTION elements with variables */
+const header = document.querySelector('header');
+const section = document.querySelector('section');
 
+async function Populate() {
 
-// STEP 3a: Create the asynchronous function populate()
+    const requestURL = 'https://b12lor.github.io/lesson10/js/i-scream.json';
 
-    // Introducing JavaScript Object Notation (JSON): https://json.org/
-    // STEP 4: Store the URL of a JSON file in a variable */
-    
-    // STEP 5: Use the new URL to create a new request object
-    
-    // STEP 6: Make a network request with the fetch() function, which returns a Response object
-    
-    // STEP 7: Capture the returned Response object and covert to a JSON object using json()
-    
-    // STEP 8: Output the iScream JSON object to the console 
-    
-    // STEP 9a: Invoke the populateHeader function here, then build it below
-    
-    // STEP 10a: Invoke the showTopFlavors function here, then build it below
-    
+    const myRequest = new Request(requestURL);
 
+    const response = await fetch(myRequest);
 
-// STEP 3b: Call the populate() function
+    const iScream = await response.json();
 
+    console.log(iScream);
 
-/* STEP 9b: Build out the populateHeader() function */
-function populateHeader() {
-    // Create the H1 element
-    
-    // Grab the company name from the JSON object and use it for the text node
-    
-    // Inject the complete H1 element into the DOM, inside the HEADER
-    
-};
-/* STEP 10b: Assemble the showTopFlavors() function */
-function showTopFlavors() {
-    // STEP 10c: Attache the JSON topFlavors object to a variable
-    //let topFlavors = jsonObj.topFlavors;
-    // STEP 10d: Loop through the topFlavors object
-    for (let i = 0; i < topFlavors.length; i ++) {
-        // STEP 10e: build HTML elements for the content
-        
+    populateHeader(iScream);
+    showTopFlavors(iScream);
+}
 
-        // STEP 10f: Set the textContent property for each of the above elements (except the UL), based on the JSON content
-        
+Populate();
 
-        // STEP 10g: Build a loop for the ingredients array in the JSON
-        
-            // add the ingredient to the UL
+function populateHeader(jsonObj) {
+    const headerH1 = document.createElement('h1');
+    headerH1.textContent = jsonObj.companyName;
+    header.appendChild(headerH1);
+}
 
-        // STEP 10h: Append each of the above HTML elements to the ARTICLE element
-        
-        // STEP 10i: Append each complete ARTICLE element to the SECTION element
-        
-    };
-};
-// STEP 11: The instructor will edit the JSON file - refresh your page to see the updated content
+function showTopFlavors(jsonObj) {
 
-// STEP 12: Change the URL in STEP 3 to point to the JSON file in the local /js folder in order to prepare for today's lab
+    let topFlavors = jsonObj.topFlavors;
 
-// This page inspired by and adapted from https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Objects/JSON
+    for (let i = 0; i < topFlavors.length; i++) {
 
-// A special thanks to https://openclipart.org/detail/285225/ice-cream-cones for the awesome ice cream cone illustrations
+        let article = document.createElement('article');
+        let h2 = document.createElement('h2');
+        let img = document.createElement('img');
+        let ul = document.createElement('ul');
+
+        h2.textContent = topFlavors[i].name;
+
+        img.setAttribute('src', 'https://b12lor.github.io/lesson10/images/' + topFlavors[i].image);
+
+        let ingredients = topFlavors[i].ingredients;
+
+        for (let j = 0; j < ingredients.length; j++) {
+            let li = document.createElement('li');
+            li.textContent = ingredients[j];
+            ul.appendChild(li);
+        }
+
+        article.appendChild(h2);
+        article.appendChild(img);
+        article.appendChild(ul);
+
+        section.appendChild(article);
+    }
+}
